64c04031c45d88d71f2fdccc23c3bcc0fac7dd38
Ferdous Hasan <49593369+ferdoushasanpr@users.noreply.github.com>
2026-06-16 22:51:44 +0600

    Add files via upload

---
+++ insomnia-cure.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/148/A
+
+#include <iostream>
+
+int main() {
+    int k, l, m, n, d, r = 0;
+    
+    std::cin >> k >> l >> m >> n >> d;
+    
+    for(int i = 1; i <= d; i++) {
+        if(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+            r++;
+        }
+    }
+    
+    std::cout << r;
+
+    return 0;
+}
